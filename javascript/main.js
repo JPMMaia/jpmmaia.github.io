02c413d3f1664d39085cec93e8f897c4314072dd
jpmmaia <jpmmaia@gmail.com>
2016-08-30 00:32:36 +0100

    - Working on the about section. -- Adding tabs.

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,9 @@
 window.onload = function()
 {
     w3IncludeHTML();
+
+    $("#about-modal-body-1").onclick = function()
+    {
+        $("#about-modal-body-1").addClass("animation-fade-out");
+    };
 };
