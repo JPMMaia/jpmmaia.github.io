db4ccf5da5dfad8870e461134c64c08188dbf281
jpmmaia <jpmmaia@gmail.com>
2016-08-31 01:53:53 +0100

    - Splitting css code across several files. - Working on the skills section.

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,4 @@
 window.onload = function()
 {
     w3IncludeHTML();
-
-    $("#about-modal-body-1").onclick = function()
-    {
-        $("#about-modal-body-1").addClass("animation-fade-out");
-    };
 };
